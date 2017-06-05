bb6bdfedbb38d6ffe3bf7df5cfd4d005e4f7b66e
alanrods <alanrods@outlook.com>
2017-06-05 15:55:34 -0500

    Sinónimos sitio 1 AJRGBD
    LEL

---
+++ synonym/main_synonym.sql
@@ -0,0 +1,24 @@
+--@Author: Moreno Tagle Ivan Raphael 
+--Rodriguez Garcia Alan Julian
+--@Description: main creacion de sinonimos
+
+Prompt Creacion de sinonimos en AJRGBD SITIO 1
+
+connect ilap/ilap@AJRGBD_S1
+@AJRGBD_S1_SYNONYM.sql
+
+Prompt Creacion de sinonimos en AJRGBD SITIO 2
+
+connect ilap/ilap@AJRGBD_S2
+@AJRGBD_S2_SYNONYM.sql
+
+Prompt Creacion de sinonimos en RIMTBD SITIO 1
+
+connect ilap/ilap@RIMTBD_S1
+@RIMTBD_S1_SYNONYM.sql
+
+Prompt Creacion de sinonimos en RIMTBD SITIO 2
+
+connect ilap/ilap@RIMTBD_S1
+@RIMTBD_S2_SYNONYM.sql
+

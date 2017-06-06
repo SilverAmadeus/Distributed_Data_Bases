9d6fc71c5a06d93a0c699737d7eb7a30de6f7891
SilverAmadeus <ivanfenix_117@hotmail.com>
2017-06-05 20:16:53 -0500

    FFS

--- USUARIOS.sql
+++ USUARIOS.sql
@@ -1,35 +1,22 @@
 prompt Conectando a AJRGBD_S1
-prompt Password:
-connect sys/&&syspass@AJRGBD_S1
+connect sys/system@AJRGBD_S1 as sysdba
 
-create user ilap identified by ilap quota unlimited on users;
-grant create session, create table, create synonym, create sequence, create procedure,
-	create trigger, create view, create database link, create type,
-	create or replace directory tmp_dir as '/tmp', read on directory tmp_dir to ilap;
+@@grants.sql
 
 prompt Conectando a AJRG_S2
-prompt Password:
-connect sys/&&syspass@AJRGBD_S2
+connect sys/system@AJRGBD_S2 as sysdba
 
-create user ilap identified by ilap quota unlimited on users;
-grant create session, create table, create synonym, create sequence, create procedure,
-	create trigger, create view, create database link, create type,
-	create or replace directory tmp_dir as '/tmp', read on directory tmp_dir to ilap;
+@@grants.sql
 
 prompt Conectando a RIMTBD_S1
-prompt Password:
-connect sys/&&syspass@RIMTBD_S1
+connect sys/system@RIMTBD_S1 as sysdba
 
-create user ilap identified by ilap quota unlimited on users;
-grant create session, create table, create synonym, create sequence, create procedure,
-	create trigger, create view, create database link, create type,
-	create or replace directory tmp_dir as '/tmp', read on directory tmp_dir to ilap;
+@@grants.sql
 
 prompt Conectando a RIMTBD_S2
-prompt Password:
-connect sys/&&syspass@RIMTBD_S2
+connect sys/system@RIMTBD_S2 as sysdba
 
-create user ilap identified by ilap quota unlimited on users;
-grant create session, create table, create synonym, create sequence, create procedure,
-	create trigger, create view, create database link, create type,
-	create or replace directory tmp_dir as '/tmp', read on directory tmp_dir to ilap;
+@grants.sql
+
+prompt Listo
+exit

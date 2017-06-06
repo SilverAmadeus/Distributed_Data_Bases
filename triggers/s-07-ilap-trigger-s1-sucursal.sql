cb3cc4c14e7ea8f1898004fe25428a9697ba3063
SilverAmadeus <ivanfenix_117@hotmail.com>
2017-06-06 16:52:00 -0500

    Trigger historico corregido

--- triggers/s-07-ilap-trigger-s1-sucursal.sql
+++ triggers/s-07-ilap-trigger-s1-sucursal.sql
@@ -14,7 +14,7 @@ instead of insert or update or delete on sucursal
 	
 	 	 	if :new.es_taller not in (0,1) or :new.es_venta not in (0,1) then
 	 	 		raise_application_error(-20003, 'valor invalido para es_taller o es_venta '||:new.es_venta ||' <=> '
-	 	 			||new.es_taller||' se esperaba: 0,1');
+	 	 			||:new.es_taller||' se esperaba: 0,1');
 	 	 	end if;
 	 	 	
 	 	 	if substr(:new.clave,3,2) = 'NO' or (:new.es_taller = 1 and :new.es_venta = 1) then
@@ -23,23 +23,28 @@ instead of insert or update or delete on sucursal
  	 			values(:new.sucursal_id,:new.clave,:new.nombre,:new.latitud,:new.longitud,:new.url,:new.es_venta,
  	 				:new.es_taller);
 
- 	 			elif substr(:new.clave,3,2)= 'EA' and (:new.es_taller = 0 and :new.es_venta = 0) then
+ 	 		elsif substr(:new.clave,3,2)= 'EA' and (:new.es_taller = 0 and :new.es_venta = 0) then
 	
-	 	 			insert into sucursal_f2 (sucursal_id,clave,nombre,latitud,longitud,url,es_venta,es_taller)
-	 	 				values(:new.sucursal_id,:new.clave,:new.nombre,:new.latitud,:new.longitud,:new.url,:new.es_venta,
+	 			insert into sucursal_f2 (sucursal_id,clave,nombre,latitud,longitud,url,es_venta,es_taller)
+ 	 				values(:new.sucursal_id,:new.clave,:new.nombre,:new.latitud,:new.longitud,:new.url,:new.es_venta,
+ 	 					:new.es_taller);
+	
+	 		elsif substr(:new.clave,3,2)='WS' and (:new.es_taller = 0 and :new.es_venta = 0) then
+	
+	 			insert into sucursal_f3 (sucursal_id,clave,nombre,latitud,longitud,url,es_venta,es_taller)
+	 				values(:new.sucursal_id,:new.clave,:new.nombre,:new.latitud,:new.longitud,:new.url,:new.es_venta,
 	 	 					:new.es_taller);
 	
-	 	 		elif substr(:new.clave,3,2)='WS' and (:new.es_taller = 0 and :new.es_venta = 0) then
+	 		elsif substr(:new.clave,3,2)='SO' and (:new.es_taller = 0 and :new.es_venta = 0) then
 	
-	 	 			insert into sucursal_f3 (sucursal_id,clave,nombre,latitud,longitud,url,es_venta,es_taller)
-	 	 				values(:new.sucursal_id,:new.clave,:new.nombre,:new.latitud,:new.longitud,:new.url,:new.es_venta,
-	 	 					:new.es_taller);
-	
-	 	 		elif substr(:new.clave,3,2)='SO' and (:new.es_taller = 0 and :new.es_venta = 0) then
-	
-	 	 			insert into sucursal_f4 (sucursal_id,clave,nombre,latitud,longitud,url,es_venta,es_taller)
-	 	 				values(:new.sucursal_id,:new.clave,:new.nombre,:new.latitud,:new.longitud,:new.url,:new.es_venta,
+	 			insert into sucursal_f4 (sucursal_id,clave,nombre,latitud,longitud,url,es_venta,es_taller)
+	 	 			values(:new.sucursal_id,:new.clave,:new.nombre,:new.latitud,:new.longitud,:new.url,:new.es_venta,
 	 		 				:new.es_taller);
+
+	 	 	else 
+	 			raise_application_error(-20004, 'Error de integridad para SUCURSAL '
+	 	 				||'Valores invalidos para CLAVE, ES_TALLER, ES_VENTA: '
+	 	 				||:new.clave||','||:new.es_venta||' , '||:new.es_taller);
  	 		end if;
 
  	 		when updating then
@@ -50,3 +55,4 @@ instead of insert or update or delete on sucursal
  	 	end case;
  	end;
 /
+show errors

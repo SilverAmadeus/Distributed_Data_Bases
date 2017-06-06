e0606df357a9432a30d433f8affd232c27673f34
alanrods <alanrods@outlook.com>
2017-06-06 08:08:17 -0500

    Test Synonyms

---
+++ synonym/test_synonym_full.sql
@@ -0,0 +1,25 @@
+--@Author: Moreno Tagle Ivan Raphael 
+--Rodriguez Garcia Alan Julian
+--@Description: main creacion de sinonimos
+
+Prompt Creacion de sinonimos en AJRGBD SITIO 1
+
+connect ilap/ilap@AJRGBD_S1
+@test_synonym.sql
+
+Prompt Creacion de sinonimos en AJRGBD SITIO 2
+
+connect ilap/ilap@AJRGBD_S2
+@test_synonym.sql
+
+Prompt Creacion de sinonimos en RIMTBD SITIO 1
+
+connect ilap/ilap@RIMTBD_S1
+@test_synonym.sql
+
+Prompt Creacion de sinonimos en RIMTBD SITIO 2
+
+connect ilap/ilap@RIMTBD_S1
+@test_synonym.sql
+
+Prompt Mission Complete!

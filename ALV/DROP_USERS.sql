e87198f6150e8adcb456e8426c343da7f2bc28dd
SilverAmadeus <ivanfenix_117@hotmail.com>
2017-06-07 03:57:34 -0500

    as sysdba

--- ALV/DROP_USERS.sql
+++ ALV/DROP_USERS.sql
@@ -1,15 +1,15 @@
-connect sys/system@ajrgbd_s1
+connect sys/system@ajrgbd_s1 as sysdba
 prompt Borrando datos en AJRGBD_S1
 drop user ilap cascade;
 
-connect sys/system@ajrgbd_s2
+connect sys/system@ajrgbd_s2 as sysdba
 prompt Borrando datos en AJRGBD_S2
 drop user ilap cascade;
 
-connect sys/system@rimtbd_s1
+connect sys/system@rimtbd_s1 as sysdba
 prompt Borrando datos en RIMTBD_S1
 drop user ilap cascade;
 
-connect sys/system@rimtbd_s2
+connect sys/system@rimtbd_s2 as sysdba
 prompt Borrando datos en RIMTBD_S2
 drop user ilap cascade;

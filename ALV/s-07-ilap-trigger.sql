0f6498e24befc12f94cfdce397fc14805e84962d
SilverAmadeus <ivanfenix_117@hotmail.com>
2017-06-07 03:23:49 -0500

    It is done

--- ALV/s-07-ilap-trigger.sql
+++ ALV/s-07-ilap-trigger.sql
@@ -2,7 +2,7 @@
 --Rodriguez Garcia Alan Julian
 --@Description: main creacion de Triggers
 
-Prompt Creacion de sinonimos en AJRGBD SITIO 1
+Prompt Creacion de triggers en AJRGBD SITIO 1
 
 connect ilap/ilap@AJRGBD_S1
 @triggers/laptop_s1.sql
@@ -13,10 +13,10 @@ connect ilap/ilap@AJRGBD_S1
 @triggers/laptop_inventario_s1.sql
 @triggers/historico_status_s1.sql
 
-Prompt Creacion de sinonimos en AJRGBD SITIO 2
+Prompt Creacion de triggers en AJRGBD SITIO 2
 
 connect ilap/ilap@AJRGBD_S2
-@triggers/laptop_s1.sql
+@triggers/laptop_s2.sql
 @triggers/sucursal_trigger.sql
 @triggers/sucursal_venta_s2.sql
 @triggers/sucursal_taller_s2.sql
@@ -24,10 +24,10 @@ connect ilap/ilap@AJRGBD_S2
 @triggers/laptop_inventario_s1.sql
 @triggers/historico_status_s2.sql
 
-Prompt Creacion de sinonimos en RIMTBD SITIO 1
+Prompt Creacion de triggers en RIMTBD SITIO 1
 
 connect ilap/ilap@RIMTBD_S1
-@triggers/laptop_s1.sql
+@triggers/laptop_s3.sql
 @triggers/sucursal_trigger.sql
 @triggers/sucursal_venta_s3.sql
 @triggers/sucursal_taller_s3.sql
@@ -35,10 +35,10 @@ connect ilap/ilap@RIMTBD_S1
 @triggers/laptop_inventario_s3.sql
 @triggers/historico_status_s1.sql
 
-Prompt Creacion de sinonimos en RIMTBD SITIO 2
+Prompt Creacion de triggers en RIMTBD SITIO 2
 
 connect ilap/ilap@RIMTBD_S1
-@triggers/laptop_s1.sql
+@triggers/laptop_s4_local.sql
 @triggers/sucursal_trigger.sql
 @triggers/sucursal_venta_s4.sql
 @triggers/sucursal_taller_s4.sql
